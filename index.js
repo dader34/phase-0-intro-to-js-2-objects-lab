99ea9f24589142e26f732ec4f361f4643d40d4e4
Danner Baumgartner <danner.baumgartner@gmail.com>
2023-08-16 17:37:32 -0700

    Passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+let employee = {
+    name : "Default",
+    streetAddress : "Fake St"
+}
+
+function updateEmployeeWithKeyAndValue(e,k,v){
+    let newemp = {...e}
+    newemp[k] = v
+    return(newemp)
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(e,k,v){
+    e[k] = v
+    return(e)
+}
+function deleteFromEmployeeByKey(e,k,v){
+    let newemp = {...e}
+    delete newemp[k]
+    return(newemp)
+}
+function destructivelyDeleteFromEmployeeByKey(e,k,v){
+    delete e[k]
+    return(e)
+}
